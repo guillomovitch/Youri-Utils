ecd66f5ab94347a43a3b9b7f691cdbcb851f2442
Guillaume Rousse <guillomovitch@gmail.com>
2005-03-28 15:37:20 +0000

    drop get_rpm function, obsoleted by Hdlist use transfer analysis function to Youri::Check::Utils documentation fixes

--- lib/Youri/Utils.pm
+++ lib/Youri/Utils.pm
@@ -19,15 +19,14 @@ use warnings;
 our @EXPORT = qw(
     send_mail
     create_instance
-    get_rpm
-    analysis
 );
 
-=head2 send_mail(I<$config>, I<$mail>)
+=head2 send_mail(I<$mail>, I<$path>, I<$test>)
 
 Send a mail.
-I<$config> is an AppConfig object used to determine test status and MTA path.
 I<$mail> is a Mime::Entity object representing the mail itself.
+I<$path> is the path to the MTA.
+I<$test> is a test flag.
 
 =cut
 
@@ -43,11 +42,11 @@ sub send_mail {
     }
 }
 
-=head2 create_instance(I<$prefix>, I<$string>)
+=head2 create_instance(class => I<$class>, I<%options>)
 
 Create an instance of a class at runtime.
-I<$prefix> is appended to class name.
-I<$string> is a stringified representation of the class constructor's call.
+I<$class> is the class name.
+I<%options> are passed to the class constructor.
 Returns the class instance.
 
 =cut
@@ -70,40 +69,6 @@ sub create_instance {
     # instantiate
     no strict 'refs';
     return $class->new(%options);
-}
-
-=head2 get_rpm(I<$file>)
-
-Get a rpm object from a file.
-I<$file> is the rpm file.
-Returns an URPM::Package object.
-
-=cut
-
-sub get_rpm {
-    my ($file) = @_;
-
-    my $urpm = URPM->new();
-    $urpm->parse_rpm($file, keep_all_tags => 1);
-    return $urpm->{depslist}->[0];
-}
-
-sub analysis {
-    my ($package) = @_;
-
-    my $arch = $package->arch();
-
-    my ($name, $unit);
-    if ($arch eq 'src') {
-	$name = $package->name();
-	$unit = $name;
-    } else {
-	$package->sourcerpm() =~ /^(\S+)-[^-]+-[^-]+\.src\.rpm$/;
-	$name = $1;
-	$unit = $package->name();
-    }
-
-    return $name, $arch, $unit;
 }
 
 1;
